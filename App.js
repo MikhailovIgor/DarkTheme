a5ad4bfabc370294729ef81f8533e5ffbb9cb56b
Igor.Mikhailov <iomihailov@gmail.com>
2020-07-13 17:43:30 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import themeReducer from './redux/themeReducer';
+
+import HomeScreen from './screens/HomeScreen';
+
+const store = createStore(
+  combineReducers({themeReducer}),
+  applyMiddleware(thunk),
+);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
+}
---
+++ Theme.js
@@ -0,0 +1,16 @@
+export const darkTheme = {
+  mode: 'dark',
+  PRIMARY_BACKGROUND_COLOR: '#212121',
+  PRIMARY_TEXT_COLOR: '#FFFFFF',
+  PRIMARY_BUTTON_COLOR: '#23A8D9',
+  PRIMARY_BUTTON_TEXT_COLOR: '#FFFFFF',
+  STATUS_BAR_STYLE: 'dark-content',
+};
+export const lightTheme = {
+  mode: 'light',
+  PRIMARY_BACKGROUND_COLOR: '#FFFFFF',
+  PRIMARY_TEXT_COLOR: '#212121',
+  PRIMARY_BUTTON_COLOR: '#8022D9',
+  PRIMARY_BUTTON_TEXT_COLOR: '#FFFFFF',
+  STATUS_BAR_STYLE: 'default',
+};
